df3e809a4325ef4c134255db9ae50171081d950e
fate1999 <davyding1999@gmail.com>
2023-06-26 11:43:26 -0500

    Configure for RTD
    Adding necessary files for RTD project

---
+++ source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'impactdb-api'
+copyright = '2023, Garrett Roell, Haodong Ding'
+author = 'Garrett Roell, Haodong Ding'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['nbsphinx', 'myst_nb']
+source_suffix = ['.rst', '.ipynb', '.html']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
+language = 'English'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+
+html_static_path = ['_static']
+
+master_doc = 'index'
---
+++ source/index.rst
@@ -0,0 +1,23 @@
+.. impactdb-api documentation master file, created by
+   sphinx-quickstart on Mon Jun 26 11:36:11 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to impactdb-api's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   Home <self>
+   description
+   demo
+
+Indices and tables
+==================
+
+.. * :ref:`genindex`
+.. * :ref:`modindex`
+
+* :ref:`search`
